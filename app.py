a3900a7555cf0748fc5de794e93ab52fff8cbdb9
LUCY0299 <85356981+LUCY0299@users.noreply.github.com>
2022-12-21 21:19:51 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 21:16:35 2021
+
+@author: Ivan
+版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+
+Line Bot聊天機器人
+第一章 Line Bot申請與串接
+Line Bot機器人串接與測試
+"""
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('htjWDxzLU4GiknoZdIVpi3dpBP5FB7ylyZsMkTST/i8zTkan0ax4crF9wPGc5bbTiyLV/4NpBTNwWrC6SBd8mQ1voq5jwnC4FiL5Ya7vP7CgPndtZxTfc3dtaKqiCXFFc1ZiQFLfAaJUTwnvvxvF0gdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('184efa5ff123ce7d254240a6fa7f9e47')
+
+line_bot_api.push_message('U3f114fe291053aab59ab469670faf458', TextSendMessage(text='你可以開始了'))
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
